62099a2406693fbb6b61c434d31eff5a2bd69071
Joe Fowler <scrumplesplunge@gmail.com>
2020-12-02 21:13:27 +0000

    Update the fetch script to grab from RNPValinor, too.
    Right now the files have different names so I'm renaming them after
    import.

--- src/fetch-puzzles.sh
+++ src/fetch-puzzles.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 users=(
-  agata-borkowska-clark
-  JoBoCl
-  Scrumplesplunge
+  agata-borkowska-clark:aoc2020:.
+  JoBoCl:aoc2020:.
+  Scrumplesplunge:aoc2020:.
+  RNPValinor:adventcode:_2020
 )
 
 function fetch {
   local user="${1?}"
+  local repository="${2?}"
   if [[ -d "/tmp/$user-aoc2020" ]]; then
     # Try `git pull` instead, since it may be faster.
     (cd "/tmp/$user-aoc2020"; git pull) && return 0;
     # If that fails, start from scratch.
     rm -rf "/tmp/$user-aoc2020"
   fi
-  git clone "git@github.com:$user/aoc2020.git" "/tmp/$user-aoc2020"
+  git clone "git@github.com:$user/$repository.git" "/tmp/$user-aoc2020"
 }
 
 function copy {
   local user="${1?}"
-  rsync -rv "/tmp/$user-aoc2020/puzzles/" "puzzles/"
+  local directory="${2?}"
+  rsync -rv "/tmp/$user-aoc2020/$directory/puzzles/" "puzzles/"
 }
 
-for user in "${users[@]}"; do
-  fetch "$user" && copy "$user"
+for info in "${users[@]}"; do
+  IFS=: read user repository directory <<< "$info"
+  echo "git@github.com:$user/$repository/$directory/puzzles"
+  fetch "$user" "$repository" && copy "$user" "$directory"
 done
