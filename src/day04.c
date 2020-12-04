b380b4249298e4a38b37e03b57650a2b050b9f0a
Joe Fowler <scrumplesplunge@gmail.com>
2020-12-04 18:29:27 +0000

    Remove many uses of strlen.

--- src/day04.c
+++ src/day04.c
@@ -64,12 +64,14 @@ enum key {
 struct entry {
   enum key key;
   const char* value;
+  int value_length;
 };
 
 // Read a single passport entry into the provided struct. `key` and `value` will
 // be updated to point at null-terminated strings for the key and value
 // respectively. The `key` will always be of length 3, but the value can have
-// arbitrary length.
+// arbitrary length. If there are no more entries in this passport, the entry
+// key will be set to 0.
 static char* read_entry(char* i, struct entry* out) {
   const char* key = i;
   if (*i == '\n') {
@@ -83,26 +85,27 @@ static char* read_entry(char* i, struct entry* out) {
   i += 4;
   out->value = i;
   while (!is_whitespace(*i)) i++;
+  out->value_length = i - out->value;
   *i = '\0';
   return i + 1;
 }
 
 static char buffer[32768];
 
-static _Bool check_year(const char* year, int min, int max) {
-  if (strlen(year) != 4) return 0;
+static _Bool check_year(struct entry* e, int min, int max) {
+  if (e->value_length != 4) return 0;
   int value;
-  if (read_int(year, &value) != year + 4) return 0;
+  if (read_int(e->value, &value) != e->value + 4) return 0;
   return min <= value && value <= max;
 }
 
-static _Bool check_height(const char* height) {
-  const int len = strlen(height);
-  if (len < 3) return 0;
-  const _Bool is_cm = height[len - 2] == 'c' && height[len - 1] == 'm';
-  const _Bool is_in = height[len - 2] == 'i' && height[len - 1] == 'n';
+static _Bool check_height(struct entry* e) {
+  if (e->value_length < 3) return 0;
+  const char* const unit = e->value + e->value_length - 2;
+  const _Bool is_cm = unit[0] == 'c' && unit[1] == 'm';
+  const _Bool is_in = unit[0] == 'i' && unit[1] == 'n';
   int value;
-  if (read_int(height, &value) != height + len - 2) return 0;
+  if (read_int(e->value, &value) != unit) return 0;
   if (is_cm) {
     return 150 <= value && value <= 193;
   } else if (is_in) {
@@ -112,18 +115,18 @@ static _Bool check_height(const char* height) {
   }
 }
 
-static _Bool check_hair(const char* color) {
-  if (strlen(color) != 7) return 0;
-  if (color[0] != '#') return 0;
+static _Bool check_hair(struct entry* e) {
+  if (e->value_length != 7) return 0;
+  if (e->value[0] != '#') return 0;
   for (int i = 1; i < 7; i++) {
-    if (!is_hex(color[i])) return 0;
+    if (!is_hex(e->value[i])) return 0;
   }
   return 1;
 }
 
-static _Bool check_eyes(const char* color) {
-  if (strlen(color) != 3) return 0;
-  switch (KEY(color)) {
+static _Bool check_eyes(struct entry* e) {
+  if (e->value_length != 3) return 0;
+  switch (KEY(e->value)) {
     case KEY3('a', 'm', 'b'):
     case KEY3('b', 'l', 'u'):
     case KEY3('b', 'r', 'n'):
@@ -137,21 +140,21 @@ static _Bool check_eyes(const char* color) {
   }
 }
 
-static _Bool check_pid(const char* value) {
-  if (strlen(value) != 9) return 0;
+static _Bool check_pid(struct entry* e) {
+  if (e->value_length != 9) return 0;
   int x;
-  return read_int(value, &x) == value + 9;
+  return read_int(e->value, &x) == e->value + 9;
 }
 
-static _Bool check_entry(struct entry* e) {
+static _Bool part2_valid(struct entry* e) {
   switch (e->key) {
-    case byr: return check_year(e->value, 1920, 2002);
-    case iyr: return check_year(e->value, 2010, 2020);
-    case eyr: return check_year(e->value, 2020, 2030);
-    case hgt: return check_height(e->value);
-    case hcl: return check_hair(e->value);
-    case ecl: return check_eyes(e->value);
-    case pid: return check_pid(e->value);
+    case byr: return check_year(e, 1920, 2002);
+    case iyr: return check_year(e, 2010, 2020);
+    case eyr: return check_year(e, 2020, 2030);
+    case hgt: return check_height(e);
+    case hcl: return check_hair(e);
+    case ecl: return check_eyes(e);
+    case pid: return check_pid(e);
     case cid: return 0;
     default: return 0;
   }
@@ -170,7 +173,7 @@ static char* check_passport(
     i = read_entry(i, &entry);
     if (!entry.key) break;
     part1 += entry.key != cid;
-    part2 += check_entry(&entry);
+    part2 += part2_valid(&entry);
   }
   out->part1 += part1 == 7;
   out->part2 += part2 == 7;
@@ -178,7 +181,6 @@ static char* check_passport(
 }
 
 int main() {
-  // Parse the input.
   int len = read(STDIN_FILENO, buffer, sizeof(buffer));
   if (len < 0) die("read");
   char* i = buffer;
