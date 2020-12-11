cf0ba5bda6b2e02f3c55bf5595158aeedc700a06
Joe Fowler <scrumplesplunge@gmail.com>
2020-12-11 21:27:58 +0000

    Cleaner solution.

--- src/day11.c
+++ src/day11.c
@@ -21,148 +21,159 @@ static void print_int(int x) {
   write(STDOUT_FILENO, buffer + i, 16 - i);
 }
 
-char input[12288];
-int size, width, height;
+enum { max_size = 128 };
+
+// Grid dimensions. All instances of `grid` will use this size.
+int grid_width, grid_height;
+
+enum cell { floor, seat, person };
+struct grid {
+  char cells[max_size][max_size];
+};
+
+struct grid input;
 
 static void read_input() {
-  char temp[12288];
-  const int input_size = read(STDIN_FILENO, temp, sizeof(temp));
-  if (input_size <= 0) die("read");
-  if (temp[input_size - 1] != '\n') die("newline");
+  char temp[65536];
+  const int length = read(STDIN_FILENO, temp, sizeof(temp));
+  if (length <= 0) die("read");
+  if (temp[length - 1] != '\n') die("newline");
   const char* i = temp;
   while (*i != '\n') i++;
-  const int input_width = i - temp;
-  const int input_stride = input_width + 1;
-  if (input_size % input_stride != 0) die("shape");
-  const int input_height = input_size / input_stride;
-  // Move the input so that it has a border of floor.
-  const int stride = input_stride + 2;
-  memset(input, '.', (input_width + 3) * (input_height + 2));
-  for (int y = 0; y < input_height; y++) {
-    const char* source = temp + y * input_stride;
-    char* destination = input + (y + 1) * stride + 1;
-    memcpy(destination, source, input_width);
-  }
-  width = input_width + 2;
-  height = input_height + 2;
-  size = (width + 1) * height;
+  const int width = i - temp;
+  const int stride = width + 1;
+  if (length % stride != 0) die("shape");
+  const int height = length / stride;
+  // Move the input into the grid. Add a border of floor around the edges to
+  // simplify the logic for handling them.
   for (int y = 0; y < height; y++) {
-    input[y * stride + width] = '\n';
+    for (int x = 0; x < width; x++) {
+      char* out = &input.cells[y + 1][x + 1];
+      switch (temp[y * stride + x]) {
+        case 'L':
+          *out = seat;
+          break;
+        case '#':
+          *out = person;
+          break;
+        case '.':
+          *out = floor;
+          break;
+        default:
+          die("bad input");
+      }
+    }
   }
+  grid_width = width + 2;
+  grid_height = height + 2;
 }
 
-char buffers[2][12288];
+struct grid buffers[2];
 static int part1() {
-  memcpy(buffers[0], input, size);
-  memcpy(buffers[1], input, size);
+  memcpy(&buffers[0], &input, sizeof(input));
   _Bool changed = 1;
-  const int stride = width + 1;
   // Iterate until the state does not change.
   for (int round = 0; changed; round++) {
     changed = 0;
     const _Bool parity = round % 2;
-    const char* const source = buffers[parity];
-    char* const destination = buffers[1 - parity];
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        const char* cell = &source[y * stride + x];
+    const struct grid* source = &buffers[parity];
+    struct grid* const destination = &buffers[1 - parity];
+    for (int y = 1; y < grid_height - 1; y++) {
+      for (int x = 1; x < grid_width - 1; x++) {
         int adjacent = 0;
         for (int dy = -1; dy <= 1; dy++) {
           for (int dx = -1; dx <= 1; dx++) {
             if (dx == 0 && dy == 0) continue;
-            if (cell[stride * dy + dx] == '#') adjacent++;
+            if (source->cells[y + dy][x + dx] == person) adjacent++;
           }
         }
-        if (*cell == 'L' && adjacent == 0) {
+        const char cell = source->cells[y][x];
+        if (cell == seat && adjacent == 0) {
           changed = 1;
-          destination[y * stride + x] = '#';
-        } else if (*cell == '#' && adjacent >= 4) {
+          destination->cells[y][x] = person;
+        } else if (cell == person && adjacent >= 4) {
           changed = 1;
-          destination[y * stride + x] = 'L';
+          destination->cells[y][x] = seat;
         } else {
-          destination[y * stride + x] = *cell;
+          destination->cells[y][x] = source->cells[y][x];
         }
       }
     }
   }
   int total = 0;
-  for (int y = 0; y < height; y++) {
-    for (int x = 0; x < width; x++) {
-      if (buffers[0][y * stride + x] == '#') total++;
+  for (int y = 1; y < grid_height - 1; y++) {
+    for (int x = 1; x < grid_width - 1; x++) {
+      if (buffers[0].cells[y][x] == person) total++;
     }
   }
   return total;
 }
 
-char direction_buffers[8][12288];
+// occupied[y][x][i] is true if someone at (x, y) can see an occupied seat in
+// direction i. If there is an unoccupied seat in the way, or if there is no
+// seat in that direction, it will be false.
+_Bool occupied[max_size][max_size][8];
+
 static int part2() {
-  const int stride = width + 1;
-  memcpy(buffers[0], input, size);
-  memcpy(buffers[1], input, size);
-  for (int i = 0; i < 8; i++) {
-    memset(direction_buffers[i], '.', size);
-    for (int j = 0; j < size; j += stride) {
-      direction_buffers[i][j + width] = '\n';
-    }
-  }
+  memcpy(&buffers[0], &input, sizeof(input));
   _Bool changed = 1;
   // Iterate until the state does not change.
   for (int round = 0; changed; round++) {
     changed = 0;
     const _Bool parity = round % 2;
-    const char* const source = buffers[parity];
-    char* const destination = buffers[1 - parity];
-    // Compute the direction buffers.
+    const struct grid* const source = &buffers[parity];
+    struct grid* const destination = &buffers[1 - parity];
+    // Update the occupied array.
     const int directions[8][2] = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0},
                                   {1, 0},   {-1, 1}, {0, 1},  {1, 1}};
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 4; i++) {
       const int dx = directions[i][0], dy = directions[i][1];
-      if (i < 4) {
-        for (int y = 1; y < height - 1; y++) {
-          for (int x = 1; x < width - 1; x++) {
-            int x2 = x + dx, y2 = y + dy;
-            const char obstruction = source[y2 * stride + x2];
-            const char behind = direction_buffers[i][y2 * stride + x2];
-            direction_buffers[i][y * stride + x] =
-                obstruction != '.' ? obstruction : behind;
-          }
+      for (int y = 1; y < grid_height - 1; y++) {
+        for (int x = 1; x < grid_width - 1; x++) {
+          int x2 = x + dx, y2 = y + dy;
+          const char obstruction = source->cells[y2][x2];
+          const char behind = occupied[y2][x2][i];
+          occupied[y][x][i] =
+              obstruction != floor ? obstruction == person : behind;
         }
-      } else {
-        for (int y = height - 2; y >= 1; y--) {
-          for (int x = width - 2; x >= 1; x--) {
-            int x2 = x + dx, y2 = y + dy;
-            const char obstruction = source[y2 * stride + x2];
-            const char behind = direction_buffers[i][y2 * stride + x2];
-            direction_buffers[i][y * stride + x] =
-                obstruction != '.' ? obstruction : behind;
-          }
+      }
+    }
+    for (int i = 4; i < 8; i++) {
+      const int dx = directions[i][0], dy = directions[i][1];
+      for (int y = grid_height - 2; y >= 1; y--) {
+        for (int x = grid_width - 2; x >= 1; x--) {
+          int x2 = x + dx, y2 = y + dy;
+          const char obstruction = source->cells[y2][x2];
+          const char behind = occupied[y2][x2][i];
+          occupied[y][x][i] =
+              obstruction != floor ? obstruction == person : behind;
         }
       }
     }
     // Compute the adjacency for the round.
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        const char* cell = &source[y * stride + x];
+    for (int y = 1; y < grid_height - 1; y++) {
+      for (int x = 1; x < grid_width - 1; x++) {
         int adjacent = 0;
         for (int i = 0; i < 8; i++) {
-          if (direction_buffers[i][y * stride + x] == '#') adjacent++;
+          adjacent += occupied[y][x][i];
         }
-        if (*cell == 'L' && adjacent == 0) {
+        const char cell = source->cells[y][x];
+        if (cell == seat && adjacent == 0) {
           changed = 1;
-          destination[y * stride + x] = '#';
-        } else if (*cell == '#' && adjacent >= 5) {
+          destination->cells[y][x] = person;
+        } else if (cell == person && adjacent >= 5) {
           changed = 1;
-          destination[y * stride + x] = 'L';
+          destination->cells[y][x] = seat;
         } else {
-          destination[y * stride + x] = *cell;
+          destination->cells[y][x] = source->cells[y][x];
         }
       }
     }
   }
   int total = 0;
-  for (int y = 0; y < height; y++) {
-    for (int x = 0; x < width; x++) {
-      if (buffers[0][y * stride + x] == '#') total++;
+  for (int y = 0; y < grid_height; y++) {
+    for (int x = 0; x < grid_width; x++) {
+      if (buffers[0].cells[y][x] == person) total++;
     }
   }
   return total;
