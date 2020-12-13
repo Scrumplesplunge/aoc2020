2553ba1a7a815327f1f59b1a20e9454c36d286b4
Joe Fowler <scrumplesplunge@gmail.com>
2020-12-13 12:10:11 +0000

    Day 13 part 2 unconfirmed solution.

--- src/day13.c
+++ src/day13.c
@@ -1,3 +1,44 @@
+// GCC automatically generates calls to __udivdi3 and __umoddi3 when attempting
+// division with `unsigned long long` variables, so we need to provide
+// a definition. However, it seems that these magic symbols don't get GC'd
+// properly when unused, so uncommenting this code causes all solvers to get
+// bigger.
+
+struct divmod_result {
+  unsigned long long quotient;
+  unsigned long long remainder;
+};
+
+static __attribute__((pure)) struct divmod_result divmod(unsigned long long a,
+                                                         unsigned long long b) {
+  unsigned long long accumulator = 0;
+  unsigned long long value = b;
+  unsigned long long factor = 1;
+  while ((value << 1) <= a) {
+    value <<= 1;
+    factor <<= 1;
+  }
+  while (factor) {
+    if (value <= a) {
+      a -= value;
+      accumulator += factor;
+    }
+    value >>= 1;
+    factor >>= 1;
+  }
+  return (struct divmod_result){.quotient = accumulator, .remainder = a};
+}
+
+__attribute__((pure)) unsigned long long __udivdi3(unsigned long long a,
+                                                   unsigned long long b) {
+  return divmod(a, b).quotient;
+}
+
+__attribute__((pure)) unsigned long long __umoddi3(unsigned long long a,
+                                                   unsigned long long b) {
+  return divmod(a, b).remainder;
+}
+
 // Exit the program with a given error message.
 __attribute__((noreturn)) static void die(const char* message) {
   write(STDERR_FILENO, message, strlen(message));
@@ -6,19 +47,25 @@ __attribute__((noreturn)) static void die(const char* message) {
 }
 
 // Print an integer in decimal, followed by a newline.
-static void print_int(int x) {
-  const _Bool negative = x < 0;
-  if (!negative) x = -x;
-  char buffer[16];
-  buffer[15] = '\n';
-  int i = 15;
-  do {
-    --i;
-    buffer[i] = '0' - (x % 10);
-    x /= 10;
-  } while (x);
-  if (negative) buffer[--i] = '-';
-  write(STDOUT_FILENO, buffer + i, 16 - i);
+static void print_int64(unsigned long long x) {
+  char buffer[24] = {[23] = '\n'};
+  // Compute the decimal format one bit at a time by doubling and carrying BCD.
+  for (int i = 63; i >= 0; i--) {
+    for (int j = 0; j < 23; j++) buffer[j] *= 2;
+    if ((x >> i) & 1) buffer[22]++;
+    char carry = 0;
+    for (int j = 22; j >= 0; j--) {
+      char temp = buffer[j] + carry;
+      buffer[j] = temp % 10;
+      carry = temp / 10;
+    }
+  }
+  // Compute the most significant digit and truncate the output.
+  int i = 0;
+  while (buffer[i] == 0) i++;
+  const int start = i < 22 ? i : 22;
+  for (int j = start; j < 23; j++) buffer[j] += '0';
+  write(STDOUT_FILENO, buffer + start, 24 - start);
 }
 
 static _Bool is_digit(char c) {
@@ -45,7 +92,7 @@ static unsigned round_up(unsigned x, unsigned k) {
 
 static int part1(const char* input) {
   unsigned earliest_departure;
-  enum { max_buses = 128 };
+  enum { max_buses = 16 };
   unsigned buses[max_buses];
   int num_buses = 0;
   input = read_int(input, &earliest_departure);
@@ -71,11 +118,56 @@ static int part1(const char* input) {
   return buses[bus] * (time - earliest_departure);
 }
 
+static unsigned long long gcd(unsigned long long a, unsigned long long b) {
+  while (b != 0) {
+    const unsigned long long temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+static unsigned long long lcm(unsigned long long a, unsigned long long b) {
+  return a / gcd(a, b) * b;
+}
+
+static unsigned long long part2(const char* input) {
+  unsigned earliest_departure;
+  enum { max_buses = 128 };
+  unsigned buses[max_buses];
+  int num_buses = 0;
+  input = read_int(input, &earliest_departure);
+  if (*input != '\n') die("bad input");
+  do {
+    input++;
+    if (num_buses == max_buses) die("too large");
+    if (*input == 'x') {
+      input++;
+      buses[num_buses++] = 0;
+    } else {
+      input = read_int(input, &buses[num_buses++]);
+    }
+  } while (*input == ',');
+  if (*input != '\n') die("bad input");
+  unsigned long long earliest = 0;
+  unsigned long long period = 1;
+  for (int i = 0; i < num_buses; i++) {
+    if (buses[i] == 0) continue;
+    // Invariant: `earliest` is the earliest time when the first i buses arrive
+    // at the right minute. `period` is the amount of time until that happens
+    // again.
+    while ((earliest + i) % buses[i] != 0) earliest += period;
+    period = lcm(period, buses[i]);
+  }
+  return earliest;
+}
+
 static char buffer[256];
 int main() {
   int length = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
   if (length <= 0) die("read");
   if (buffer[length - 1] != '\n') die("newline");
   buffer[length] = '\0';
-  print_int(part1(buffer));
+  print_int64(part1(buffer));
+  print_int64(part2(buffer));
 }
