4016d206da9bf8bdd6d4dbf9e2749cd9411e6b39
Joe Fowler <scrumplesplunge@gmail.com>
2020-12-23 14:44:38 +0000

    Day 23 part 2 unconfirmed solution.

--- src/day23.c
+++ src/day23.c
@@ -5,6 +5,28 @@ __attribute__((noreturn)) static void die(const char* message) {
   exit(1);
 }
 
+// Print an integer in decimal, followed by a newline.
+static void print_int64(unsigned long long x) {
+  char buffer[24] = {[23] = '\n'};
+  // Compute the decimal format one bit at a time by doubling and carrying BCD.
+  for (int i = 63; i >= 0; i--) {
+    for (int j = 0; j < 23; j++) buffer[j] *= 2;
+    if ((x >> i) & 1) buffer[22]++;
+    char carry = 0;
+    for (int j = 22; j >= 0; j--) {
+      char temp = buffer[j] + carry;
+      buffer[j] = temp % 10;
+      carry = temp / 10;
+    }
+  }
+  // Compute the most significant digit and truncate the output.
+  int i = 0;
+  while (buffer[i] == 0) i++;
+  const int start = i < 22 ? i : 22;
+  for (int j = start; j < 23; j++) buffer[j] += '0';
+  write(STDOUT_FILENO, buffer + start, 24 - start);
+}
+
 struct node {
   struct node* next;
 };
@@ -37,16 +59,12 @@ static struct node* step(
   // write(STDOUT_FILENO, "\n", 1);
 }
 
-int main() {
-  char buffer[10];
-  const int length = read(STDIN_FILENO, buffer, sizeof(buffer));
-  if (length != 10) die("read");
-  if (buffer[9] != '\n') die("newline");
+static void part1(const char* input) {
   struct node nodes[9];
   struct node* first;
   struct node** previous = &first;
   for (int i = 0; i < 9; i++) {
-    const int value = buffer[i] - '0';
+    const int value = input[i] - '0';
     struct node* n = &nodes[value - 1];
     *previous = n;
     previous = &n->next;
@@ -61,4 +79,38 @@ int main() {
   }
   out[8] = '\n';
   write(STDOUT_FILENO, out, 9);
+}
+
+struct node nodes[1000000];
+static void part2(const char* input) {
+  struct node* first;
+  struct node** previous = &first;
+  for (int i = 0; i < 9; i++) {
+    const int value = input[i] - '0';
+    struct node* n = &nodes[value - 1];
+    *previous = n;
+    previous = &n->next;
+  }
+  for (int i = 9; i < 1000000; i++) {
+    *previous = &nodes[i];
+    previous = &nodes[i].next;
+  }
+  *previous = first;
+  struct node* current = first;
+  for (int move = 0; move < 10000000; move++) {
+    current = step(nodes, 1000000, current);
+  }
+  const struct node* const one = &nodes[0];
+  unsigned long long a = one->next - nodes + 1;
+  unsigned long long b = one->next->next - nodes + 1;
+  print_int64(a * b);
+}
+
+int main() {
+  char buffer[10];
+  const int length = read(STDIN_FILENO, buffer, sizeof(buffer));
+  if (length != 10) die("read");
+  if (buffer[9] != '\n') die("newline");
+  part1(buffer);
+  part2(buffer);
 }
