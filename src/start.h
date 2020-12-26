69df8bec808a1d28c20f33b0359df6ebcee7022e
Joe Fowler <scrumplesplunge@gmail.com>
2020-12-26 16:36:17 +0000

    Add gettimeofday.

--- src/start.h
+++ src/start.h
@@ -38,6 +38,20 @@ static __attribute__((noreturn)) void exit(int code) {
   asm volatile("int $0x80" : : "a"(1), "b"(code));
 }
 
+struct timeval {
+  unsigned tv_sec;
+  unsigned tv_usec;
+};
+
+static int gettimeofday(struct timeval* restrict tp, void* restrict tzp) {
+  int result;
+  asm volatile("int $0x80"
+               : "=a"(result)
+               : "a"(78), "b"(tp), "c"(tzp)
+               : "memory");
+  return result;
+}
+
 // Utility functions from the standard library.
 
 static unsigned int strlen(const char* c_string) {
