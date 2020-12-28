7570f1403543f5d0c25d7545f7c4aabb95d091e6
Joe Fowler <scrumplesplunge@gmail.com>
2020-12-28 14:22:12 +0000

    Move division logic out of day13.
    Only day13 uses it, but it's not really relevant to the solution.

--- src/day13.c
+++ src/day13.c
@@ -1,47 +1,7 @@
 #include "util/die.h"
+#include "util/division.h"
 #include "util/print_int64.h"
 #include "util/read_int.h"
-
-// GCC automatically generates calls to __udivdi3 and __umoddi3 when attempting
-// division with `unsigned long long` variables, so we need to provide
-// a definition. However, it seems that these magic symbols don't get GC'd
-// properly when unused, so uncommenting this code causes all solvers to get
-// bigger.
-
-struct divmod_result {
-  unsigned long long quotient;
-  unsigned long long remainder;
-};
-
-static __attribute__((pure)) struct divmod_result divmod(unsigned long long a,
-                                                         unsigned long long b) {
-  unsigned long long accumulator = 0;
-  unsigned long long value = b;
-  unsigned long long factor = 1;
-  while ((value << 1) <= a) {
-    value <<= 1;
-    factor <<= 1;
-  }
-  while (factor) {
-    if (value <= a) {
-      a -= value;
-      accumulator += factor;
-    }
-    value >>= 1;
-    factor >>= 1;
-  }
-  return (struct divmod_result){.quotient = accumulator, .remainder = a};
-}
-
-__attribute__((pure)) unsigned long long __udivdi3(unsigned long long a,
-                                                   unsigned long long b) {
-  return divmod(a, b).quotient;
-}
-
-__attribute__((pure)) unsigned long long __umoddi3(unsigned long long a,
-                                                   unsigned long long b) {
-  return divmod(a, b).remainder;
-}
 
 // Round x up to the next multiple of k.
 static unsigned round_up(unsigned x, unsigned k) {
