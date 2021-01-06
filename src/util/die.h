e0d8a44a18ec635d772e6789247ff602bb01e752
Joe Fowler <scrumplesplunge@gmail.com>
2021-01-06 17:59:39 +0000

    Remove error messages in optimized binaries.
    This significantly reduces total binary sizes.

--- src/util/die.h
+++ src/util/die.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#ifndef NDEBUG
 #include "strlen.h"
+#endif
 
 // Exit the program with an error message.
 __attribute__((noreturn)) static void die(const char* message) {
+#ifdef NDEBUG
+  (void)message;
+#else
   write(STDERR_FILENO, message, strlen(message));
   write(STDERR_FILENO, "\n", 1);
+#endif
   exit(1);
 }
